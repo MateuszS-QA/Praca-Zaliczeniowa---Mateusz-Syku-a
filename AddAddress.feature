66b6d9f113931d901a7f81dbc9401be9e2594aa2
MateuszS-QA <62853850+MateuszS-QA@users.noreply.github.com>
2020-05-07 23:20:44 +0200

    Add files via upload

---
+++ AddAddress.feature
@@ -0,0 +1,18 @@
+Feature: Login as user
+ Scenario Outline: Login to system as a pre-created user
+    Given Open browser with address: https://prod-kurs.coderslab.pl/index.php?controller=addresses
+    Then Log in using user:elftiptfinbmkemsjc@awdrt.net with password: coderslab
+    Then Choose " + Create new address"
+    Then Fill form using "example" table with fields: <alias>, <address>, <city>, <zipCode>, <phone>
+    Then Check data correctness
+    And Close device
+   Examples:
+
+
+
+
+    Examples:
+      | alias| address     | city       | zipCode| country      | phone      |
+      |Mr    |ABC str 16/5 |New York    |12-123  |United Kingdom |123 345 654 |
+
+
---
+++ AddAddressSteps.java
@@ -0,0 +1,68 @@
+package cucumber.steps;
+
+
+        import cucumber.AddAddressClass;
+        import cucumber.api.PendingException;
+        import cucumber.api.java.en.And;
+        import cucumber.api.java.en.Given;
+        import cucumber.api.java.en.Then;
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.chrome.ChromeDriver;
+        import org.openqa.selenium.support.FindBy;
+        import org.openqa.selenium.support.ui.Select;
+
+public class AddAddressSteps extends AddAddressClass {
+    private WebDriver driver;
+
+    @Given("^Open browser with address: https://prod-kurs\\.coderslab\\.pl/index\\.php\\?controller=addresses$")
+    public void OpenBrowserAndSetHomePage() {
+        System.setProperty("webdriver.chrome.driver",
+                "src/main/resources/drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://prod-kurs.coderslab.pl/index.php?controller=addresses");
+    }
+
+    @Then("^Log in using user:elftiptfinbmkemsjc@awdrt\\.net with password: coderslab$")
+    public void LogIn() {
+
+        WebElement login = driver.findElement(By.name("email"));
+        login.clear();
+        login.sendKeys("elftiptfinbmkemsjc@awdrt.net");
+        WebElement password = driver.findElement(By.name("password"));
+        password.clear();
+        password.sendKeys("coderslab");
+        WebElement submitLogin = driver.findElement(By.id("submit-login"));
+        submitLogin.click();
+
+    }
+
+    @Then("^Choose \"([^\"]*)\"$")
+    public void addNew(String arg0) throws Throwable {
+        WebElement addNew = driver.findElement(By.cssSelector(".addresses-footer > a:nth-child(1) > span:nth-child(2)"));
+        addNew.click();
+    }
+    @Then("^Fill form using \"example\" table with fields: (.*),(.*),(.*),(.*),(.*)$")
+    public void fillFormUsingTableWithFieldsAlias(String alias, String address, String city, String zipCode, String phone) throws Throwable {
+        driver.findElement(By.name("alias")).sendKeys(alias);
+        driver.findElement(By.name("address1")).sendKeys(address);
+        driver.findElement(By.name("city")).sendKeys(city);
+        driver.findElement(By.name("postcode")).sendKeys(zipCode);
+        new Select(driver.findElement(By.name("id_country"))).selectByIndex(1);
+        driver.findElement(By.name("phone")).sendKeys(phone);
+        throw new PendingException();
+    }
+    @Then("^Check data correctness$")
+    public void checkDataCorrectness() {
+        WebElement submitAddress = driver.findElement(By.name("submitAddress"));
+        submitAddress.click();
+
+
+    }
+    
+    @And("^Close device$")
+    public void closeDevice() {
+    }
+}
